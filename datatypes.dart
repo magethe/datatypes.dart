4c5257288a4b67d209d4cdc317dfc4d0a8323c64
Jermaine Magethe <100834221+magethe@users.noreply.github.com>
2024-02-28 10:45:56 +0300

    Add files via upload

---
+++ datatypes.dart
@@ -0,0 +1,28 @@
+void main() {
+  // Example of int data type
+  int age = 18;
+
+  // Example of double data type
+  double height = 7.3;
+
+  // Example of String data type
+  String name = "Jermaine Magethe";
+
+  // Example of List data type
+  List<String> hobbies = ["Reading", "Running", "Cooking"];
+
+  // Example of Map data type
+  Map<String, dynamic> person = {
+    'name': 'Kingori',
+    'age': 20,
+    'isEmployed': true,
+    'hobbies': ['Painting', 'Traveling']
+  };
+
+  // Printing values
+  print('Age: $age');
+  print('Height: $height');
+  print('Name: $name');
+  print('Hobbies: $hobbies');
+  print('Person: $person');
+}
